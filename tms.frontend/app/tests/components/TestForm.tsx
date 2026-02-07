c125c8ec47e300d1ff34a129063fdf04aee9ed85
Francisco A. Micucci <francisco.micucci@diveria.com>
2026-02-07 18:01:16 -0300

    create test

---
+++ tms.frontend/app/tests/components/TestForm.tsx
@@ -0,0 +1,79 @@
+"use client";
+import { Button } from "@/components/Button";
+import { Input } from "@/components/Input";
+import { Select } from "@/components/Select";
+import useTest from "@/store/test/test";
+import { useForm } from "react-hook-form";
+
+const TestForm = ({ onSubmit: onAfterSubmit }: { onSubmit?: () => void }) => {
+  const { createTest } = useTest();
+
+  const defaultValues = {
+    ticketId: "",
+    qaStatus: "",
+    date: new Date().toISOString().slice(0, 16),
+    version: "",
+    observations: "",
+    testCaseId: "",
+  };
+
+  const { control, handleSubmit, reset } = useForm({
+    defaultValues,
+  });
+
+  const onSubmit = async (data: any) => {
+    await createTest({
+      ticketId: data.ticketId,
+      qaStatus: data.qaStatus,
+      date: data.date,
+      version: data.version,
+      observations: data.observations,
+      testCaseId: parseInt(data.testCaseId, 10),
+    });
+
+    reset();
+
+    onAfterSubmit && onAfterSubmit();
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="flex flex-col items-stretch gap-4 min-w-[500px]"
+    >
+      <Input name="ticketId" control={control} placeholder="Ticket ID" />
+      <Input
+        name="date"
+        control={control}
+        placeholder="Fecha"
+        type="datetime-local"
+      />
+      <Input name="version" control={control} placeholder="Versión" />
+      <Select
+        name="qaStatus"
+        control={control}
+        placeholder="Estado QA"
+        options={[
+          { value: "Passed", label: "Aprobado" },
+          { value: "Failed", label: "Fallido" },
+          { value: "Pending", label: "Pendiente" },
+        ]}
+      />
+      <Input
+        name="observations"
+        control={control}
+        placeholder="Observaciones"
+      />
+      <Input
+        name="testCaseId"
+        control={control}
+        placeholder="ID del Caso de Prueba"
+      />
+      <Button type="submit" variant="primary" className="mt-2">
+        Crear
+      </Button>
+    </form>
+  );
+};
+
+export default TestForm;
--- tms.frontend/app/tests/page.tsx
+++ tms.frontend/app/tests/page.tsx
@@ -1,16 +1,30 @@
 "use client";
-import React from "react";
 import Tests from "./components/Tests";
+import TestForm from "./components/TestForm";
+import { useModal } from "@/contexts/ModalContext";
 import { Button } from "@/components/Button";
 import { Typography } from "@/components/Typography";
+import { ModalContent } from "@/components/Modal/ModalContent";
 
 const Page = () => {
+  const { show, hide } = useModal();
+
   return (
     <div>
       <div className="flex items-center justify-between py-2">
         <Typography variant="h3">Tests</Typography>
         <div>
-          <Button onClick={() => {}}>Crear Test</Button>
+          <Button
+            onClick={() =>
+              show("Crear un Test", () => (
+                <ModalContent>
+                  <TestForm onSubmit={() => hide()} />
+                </ModalContent>
+              ))
+            }
+          >
+            Crear Test
+          </Button>
         </div>
       </div>
       <Tests />
