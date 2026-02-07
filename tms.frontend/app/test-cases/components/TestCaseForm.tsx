33fa9b8ffc9a79ce47703b2b753991e5bbdcd480
Francisco A. Micucci <francisco.micucci@diveria.com>
2026-02-07 15:05:08 -0300

    install react hook form

---
+++ tms.frontend/app/test-cases/components/TestCaseForm.tsx
@@ -0,0 +1,5 @@
+const TestCaseForm = () => {
+  return <div>este es el formulario para crear y editar test cases</div>;
+};
+
+export default TestCaseForm;
--- tms.frontend/app/test-cases/page.tsx
+++ tms.frontend/app/test-cases/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import TestCases from "./components/TestCases";
+import TestCaseForm from "./components/TestCaseForm";
 
 const Page = () => {
   return (
     <div>
       <h1>Test Cases</h1>
+      <TestCaseForm />
       <TestCases />
     </div>
   );
