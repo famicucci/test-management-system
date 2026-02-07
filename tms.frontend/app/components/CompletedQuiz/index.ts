88836bedba4f65bff9e064c44a2d9f1b65523dba
Francisco A. Micucci <francisco.micucci@diveria.com>
2026-02-07 13:58:55 -0300

    remove components and change layout

--- tms.frontend/app/components/CompletedQuiz/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as CompletedQuiz } from "./CompletedQuiz";
--- tms.frontend/app/test-cases/page.tsx
+++ tms.frontend/app/test-cases/page.tsx
@@ -1,8 +1,7 @@
 import React from "react";
-import Quiz from "../components/Quiz/Quiz";
 
 const Page = () => {
-  return <Quiz />;
+  return <div>test cases page</div>;
 };
 
 export default Page;
